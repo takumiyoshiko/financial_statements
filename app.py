453b9fd6028d2b8d8aaf703f7cb3a14d1273780f
Takumi Yoshikoshi <takumiyoshiko@kyoto-u.ac.jp>
2020-12-21 21:30:21 +0900

    first commit

--- app.py
+++ app.py
@@ -141,5 +141,5 @@ def export():
         resp.headers["Content-Type"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         return resp
 
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
